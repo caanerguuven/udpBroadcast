5c4a89202ede44313df3a711641a1e7a024d8788
Caner Güven <caanerguuven@hotmail.com>
2022-01-26 15:06:29 +0300

    First init for deploying

---
+++ server/server.js
@@ -0,0 +1,68 @@
+import express from "express";
+import fs from "fs";
+import path from "path";
+import React from "react";
+import reactDomServer from "react-dom/server";
+import App from "../src/App";
+
+const appPort = 3000;
+const app = express();
+
+var drivers = [];
+app.use("^/$", (req, res, next) => {
+  fs.readFile(path.resolve("./build/index.html"), "utf-8", (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send("on Server Side error happened");
+    }
+
+    return res.send(
+      data.replace(
+        '<div id="root"></div>',
+        `<div id="root">${reactDomServer.renderToString(<App />)}</div>`
+      )
+    );
+  });
+});
+
+app.use(express.static(path.resolve(__dirname, "..", "build")));
+
+app.listen(appPort, () => {
+  console.log(`running on port ${appPort}`);
+  console.log(`Listening on port ${socketPort}`);
+});
+
+app.get("/drivers", (req, res) => {
+  res.send(JSON.stringify(drivers));
+});
+
+const socketPort = 41794;
+var dgram = require("dgram");
+const socket = dgram.createSocket(
+  {
+    type: "udp4",
+    reuseAddr: true,
+  },
+  (buffer, sender) => {
+    socket.send(buffer, sender.port, sender.address, (error) => {
+      if (error) {
+        console.log(error);
+      } else {
+        var driverObject = {
+          kind: "RESPOND",
+          senderAddress: sender.address,
+          senderPort: sender.port,
+          senderFamily: sender.family,
+        };
+
+        let existed =drivers.find(el=> el.senderAddress == driverObject.senderAddress)
+        if (existed==null) {
+          drivers.push(driverObject);
+          console.log("Length of Drivers is " + drivers.length);
+        }
+      }
+    });
+  }
+);
+
+socket.bind(socketPort);
--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useEffect, useState } from "react";
+import regeneratorRuntime from "regenerator-runtime";
+import "./App.scss";
 
 function App() {
+  const [drivers, setDrivers] = useState([]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      callDrivers()
+        .then((res) => {
+          console.log("API is ok222");
+        })
+        .catch((err) => console.log(err));
+    }, 10000);
+
+    return()=>clearInterval(interval)
+  }, []);
+
+  const callDrivers = useCallback(async () => {
+    const response = await fetch("/drivers", {
+      method: "get",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => setDrivers(res))
+      .catch((err) => console.log(err));
+
+    return response;
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row align-items-stretch">
+        {drivers &&
+          drivers?.map((el, index) => {
+            return (
+              <div key={index} className="col-md-4 mb-4">
+                <div key={index} className="custom--card small-card">
+                  <p>Address : {el.senderAddress}</p>
+                  <p>Port : {el.senderPort}</p>
+                  <p>Type : {el.senderFamily}</p>
+                  <a
+                    className="btn"
+                    href={`http://${el?.senderAddress}/LCD`}
+                    target="_blank"
+                    rel="noreferrer"
+                    style={{ marginRight: "30px" }}
+                  >
+                    Driver Link
+                  </a>
+                </div>
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 }
