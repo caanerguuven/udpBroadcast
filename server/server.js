670e9d1d5062376c30e7e80b72d45b036212fdb1
Caner Güven <caanerguuven@hotmail.com>
2022-01-27 10:19:57 +0300

    hosting tryings-5

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ var drivers = [];
   });
 });*/
 
-const buildPath = path.join(__dirname, "..", "build")
+const buildPath = path.join(__dirname, "..", "public")
 app.use(express.static(buildPath));
 
 app.listen(appPort, () => {
