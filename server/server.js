60d2714a4b8cd5aa8cf3b59560d920199efdb327
Caner Güven <caanerguuven@hotmail.com>
2022-01-27 16:48:19 +0300

    Deploy was done

--- server/server.js
+++ server/server.js
@@ -1,32 +1,20 @@
 const express = require("express");
+const { magicPacket } = require("./const.js");
+var ip = require("ip");
 const path = require("path");
 const app = express();
 
 const appPort = process.env.PORT || 5000;
 
 var drivers = [];
-/*app.use("^/$", (req, res, next) => {
-  fs.readFile(path.resolve("./build/index.html"), "utf-8", (err, data) => {
-    if (err) {
-      console.log(err);
-      return res.status(500).send("on Server Side error happened");
-    }
 
-    return res.send(
-      data.replace(
-        '<div id="root"></div>',
-        `<div id="root">${reactDomServer.renderToString(<App />)}</div>`
-      )
-    );
-  });
-});*/
-
-const buildPath = path.join(__dirname, "..", "build")
+const buildPath = path.join(__dirname, "..", "build");
 app.use(express.static(buildPath));
 
 app.listen(appPort, () => {
+  /*console.log(`running on ip ${ip.address()}`);
   console.log(`running on port ${appPort}`);
-  console.log(`Listening on port ${socketPort}`);
+  console.log(`Listening on port ${socketPort}`);*/
 });
 
 app.get("/drivers", (req, res) => {
@@ -34,32 +22,54 @@ app.get("/drivers", (req, res) => {
 });
 
 const socketPort = 41794;
-var dgram = require("dgram");
-const socket = dgram.createSocket(
-  {
-    type: "udp4",
-    reuseAddr: true,
-  },
-  (buffer, sender) => {
-    socket.send(buffer, sender.port, sender.address, (error) => {
-      if (error) {
-        console.log(error);
-      } else {
-        var driverObject = {
-          kind: "RESPOND",
-          senderAddress: sender.address,
-          senderPort: sender.port,
-          senderFamily: sender.family,
-        };
+const socketHost = "0.0.0.0";
 
-        let existed =drivers.find(el=> el.senderAddress == driverObject.senderAddress)
-        if (existed==null) {
-          drivers.push(driverObject);
-          console.log("Length of Drivers is " + drivers.length);
-        }
-      }
-    });
+var dgram = require("dgram");
+const socket = dgram.createSocket({ type: "udp4", reuseAddr: true });
+socket.bind({ port: socketPort,address:socketHost });
+
+socket.on("listening", () => {
+  var address = socket.address();
+  console.log("server listening " + address.address + ":" + address.port);
+});
+
+socket.on("error", (err) => {
+  console.log("UDP server binding error!", err);
+});
+
+socket.send(
+  magicPacket,
+  0,
+  magicPacket.length,
+  socketPort,
+  socketHost,
+  function (err) {
+    console.log(err ? err : "Hello ! Who is there?");
   }
 );
 
-socket.bind(socketPort);
+socket.on("message", function (msg, sender) {
+  console.log(
+    "server got: " + msg + " from " + sender.address + ":" + sender.port
+  );
+
+  var driverObject = {
+    kind: "RESPOND",
+    senderAddress: sender.address,
+    senderPort: sender.port,
+    senderFamily: sender.family,
+  };
+
+  let existed = drivers.find(
+    (el) => el.senderAddress == driverObject.senderAddress
+  );
+
+  console.log(":::::-> " + driverObject.senderAddress);
+  if (existed == null) {
+    if (msg.Length > 266 && msg[0] == 0x15) {
+      drivers.push(driverObject);
+      console.log(`It is ${driverObject.senderAddress} and i am here`);
+    }
+  }
+});
+
